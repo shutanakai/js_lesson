3de156f2c115eec5dbdf2e2f9c09bc981748979c
*93kC8O4PqJ <shuta.nakai@hakuhodo-ms.co.jp>
2022-11-21 23:16:17 +0900

    delete script

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,1 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const profileNextButton = document.querySelector(".profile__next");
-  const profileImage = document.querySelector(".profile__image");
-  const profileTitle = document.querySelector(".profile__title");
-  const profileDesc = document.querySelector(".profile__desc");
-
-  profileNextButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    profileImage.src =
-      "https://1.bp.blogspot.com/-_abP-kIkBWw/X9GYQHFIbBI/AAAAAAABcvA/Vdz-VP3ylPwbC7vSKqYBL3T3sa77uWK6ACNcBGAsYHQ/s400/music_guitar_stand_guitar.png";
-    profileTitle.innerHTML = "趣味";
-    profileDesc.innerHTML = "たまにギターを弾きます。";
-    profileNextButton.style.display = "none";
-  });
-});
+window.addEventListener("DOMContentLoaded", () => {});
