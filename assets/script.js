9f0a038682fc957d4cdd7337ac2905451ad82745
*93kC8O4PqJ <shuta.nakai@hakuhodo-ms.co.jp>
2022-11-21 23:47:13 +0900

    コメントの追加

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
-window.addEventListener("DOMContentLoaded", () => {
+// DOMの読み込み,CSS・画像等のリソースの読み込みが完了したら実行する
+window.addEventListener("load", () => {
   const profileNextButton = document.querySelector(".profile__next");
   const profileImage = document.querySelector(".profile__image");
   const profileTitle = document.querySelector(".profile__title");
