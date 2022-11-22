7dd6f74483f4a0b6e58bea251496a83da2fc6895
*93kC8O4PqJ <shuta.nakai@hakuhodo-ms.co.jp>
2022-11-22 14:41:47 +0900

    スクリプト書き直し

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,22 @@
 // DOMの読み込み,CSS・画像等のリソースの読み込みが完了したら実行する
 window.addEventListener("load", () => {
-  const profileNextButton = document.querySelector(".profile__next");
-  const profileImage = document.querySelector(".profile__image");
-  const profileTitle = document.querySelector(".profile__title");
-  const profileDesc = document.querySelector(".profile__desc");
+  // 検証ツールのConsoleにログとして"Hello JavaScript!"を表示する
+  console.log("Hello JavaScript!");
 
-  profileNextButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    profileImage.src =
-      "https://1.bp.blogspot.com/-_abP-kIkBWw/X9GYQHFIbBI/AAAAAAABcvA/Vdz-VP3ylPwbC7vSKqYBL3T3sa77uWK6ACNcBGAsYHQ/s400/music_guitar_stand_guitar.png";
-    profileTitle.innerHTML = "趣味";
-    profileDesc.innerHTML = "たまにギターを弾きます。";
-    profileNextButton.style.display = "none";
+  document.querySelector(".profile__title").innerHTML = "taro yamada";
+
+  document.querySelector(".profile__desc").innerHTML = "ディレクターです。";
+
+  // ボタンを取得する
+  console.log(document.querySelector(".profile__next"));
+
+  document.querySelector(".profile__next").addEventListener("click", function () {
+    // ボタンをクリックした時この中を実行する
+    console.log("ボタンがクリックされました。");
+
+    document.querySelector(".profile__title").innerHTML = "ギター";
+    document.querySelector(".profile__desc").innerHTML = "たまに弾いています。";
+    document.querySelector(".profile__image").src = "https://1.bp.blogspot.com/-_abP-kIkBWw/X9GYQHFIbBI/AAAAAAABcvA/Vdz-VP3ylPwbC7vSKqYBL3T3sa77uWK6ACNcBGAsYHQ/s400/music_guitar_stand_guitar.png";
+    document.querySelector(".profile__next").style.display = "none";
   });
 });
